a387b446dcec2efaf0f0166c608358cffd29e352
andrewIC-2019 <61808125+andrewIC-2019@users.noreply.github.com>
2020-08-11 09:44:10 -0600

    Ej Strategy

---
+++ ProyectoSoda/src/Modelo/LeerAdmins.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Andrés
+ */
+public class LeerAdmins implements StrategyLecturaArchivo{
+
+    @Override
+    public String Recuperar() {
+        //Acá iría el proceso de carga de Administradores
+        return "";
+    }
+    
+}
--- ProyectoSoda/src/Modelo/StrategyArchivo.java
+++
@@ -1,7 +0,0 @@
-
-package Modelo;
-
-public interface StrategyArchivo {
-    public String Recuperar();
-    public String Actualizar(); 
-}
