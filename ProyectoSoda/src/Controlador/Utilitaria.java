867ed92f642bdd9cd826af20d6079e43af339114
BryanSQ <62042791+BryanSQ@users.noreply.github.com>
2020-08-11 14:14:29 -0600

    Creacion de algunas clases

---
+++ ProyectoSoda/src/Controlador/Utilitaria.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import java.util.Date;
+
+/**
+ *
+ * @author bryan
+ */
+public class Utilitaria {
+    
+    public String FormatoDia(Date unDate){
+        return "";
+    }
+    
+    public String FormatoHora(Date unDate){
+        return "";
+    }
+    
+    public boolean VerificarNoEnBlanco(String linea){
+        return true;
+    }
+    
+    public boolean VerificarNumeroTelefono(String linea){
+        return true;
+    }
+    
+    public boolean VerificarCedula(String linea){
+        return true;
+    }
+        
+        
+}
---
+++ ProyectoSoda/src/Modelo/Menu.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author bryan
+ */
+public class Menu {
+    private ArrayList<Combo> listCombo= new ArrayList<>();
+    private ArrayList<Plato> listPlatillo= new ArrayList<>();
+
+    public Menu() {
+    }
+    
+    public void VerPlatos(){
+    }
+    
+    public void VerCombos(){
+    }
+    
+    public void ModificarPlato(Plato platillo){
+        
+    }
+    
+    public void RegistrarPlato(){ //tal vez no deba recibir el objeto plato
+        
+    }
+    
+    public void RegistrarCombo(Combo unCombo){
+        
+    }
+    
+    public void ModificarCombo(){
+        
+    }
+}
+
