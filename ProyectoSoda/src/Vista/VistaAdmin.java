8e800def16c8587f6178fdaf436828aa9509c413
PCesar45 <49625916+PCesar45@users.noreply.github.com>
2020-08-13 12:50:16 -0600

    Botones

--- ProyectoSoda/src/Vista/VistaAdmin.java
+++ ProyectoSoda/src/Vista/VistaAdmin.java
@@ -10,7 +10,10 @@ import Modelo.Combo;
 import Modelo.LeerCombos;
 import Modelo.LeerPlatos;
 import Modelo.Plato;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import javax.swing.JButton;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
@@ -667,10 +670,21 @@ public class VistaAdmin extends javax.swing.JFrame {
 
                 modelo.addRow(ListaColumna);
              }
-            
-        
-        
+            JButton Registar=new JButton("Registar Nuevo");  
+        Registar.setLocation(1150, 200);
+        vista.add(Registar);
+        Registar.setSize(150, 60);
          vista.setVisible(true);
+         Registar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Agregar ag=new Agregar();
+                ag.setVisible(true);
+            }
+        });
+        
+        
+         
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -678,6 +692,8 @@ public class VistaAdmin extends javax.swing.JFrame {
        
 //        
 //        //Carga los combos
+        LeerPlatos plts= new LeerPlatos();
+        plts.Recuperar();
         LeerCombos p1 = new LeerCombos();
         p1.Recuperar();
         boolean combos=true;
@@ -714,7 +730,18 @@ public class VistaAdmin extends javax.swing.JFrame {
              }
             
         }
-        vista.setVisible(true);
+        JButton Registar=new JButton("Registar Nuevo");  
+        Registar.setLocation(1150, 200);
+        vista.add(Registar);
+        Registar.setSize(150, 60);
+         vista.setVisible(true);
+         Registar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Agregar ag=new Agregar();
+                ag.setVisible(true);
+            }
+        });
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
